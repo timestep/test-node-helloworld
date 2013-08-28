64e9c40fded76d37fd4ca40cf5bbe3abdc85d7b6
timestep <jbosshellokitty@gmail.com>
2013-08-28 16:11:24 -0400

    simple refactoring

--- server.js
+++ server.js
@@ -1,13 +1,6 @@
-var http = require('http');
-
-var server = http.createServer();
-
-function handleRequest(req,res){
-	res.writeHead(200,{'content-type':'text/plain'});
-	res.write('Hello World!');
-	res.end();
-}
-
-server.on('request',handleRequest);
-
-server.listen(8080);
+require('http')
+	.createServer(function handleRequest(req,res){
+		res.writeHead(200,{'content-type':'text/plain'});
+		res.end('Hello World');
+	})
+	.listen(8080);
